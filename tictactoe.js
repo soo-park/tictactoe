86cc1346b1cb1e9def85945cffc2dd4c79da9243
soo-park <sparkstacks@gmail.com>
2017-08-07 14:58:19 -0700

    initial commit

---
+++ tictactoe.js
@@ -0,0 +1,23 @@
+
+
+console.log("yay");
+
+var Game = function() {
+  this.board = [['-', '-', '-'],['-', '-', '-'], ['-', '-', '-']]
+  this.player1 = new Player(true);
+  this.player2 = new Player(false);
+
+  return "I won"
+}
+
+var Player = function(turn) {
+  this.spot = [];
+  this.turn = turn;
+}
+
+
+
+
+var a = new Player();
+
+console.log(a);
